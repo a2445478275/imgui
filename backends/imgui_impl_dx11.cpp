8857e2c192a1f052addeca46bc03ecce402b8bf9
GodLeaveMe <godleaveme233@gmail.com>
2023-02-07 14:24:41 +0800

    fixup
    Signed-off-by: GodLeaveMe <godleaveme233@gmail.com>

--- backends/imgui_impl_dx11.cpp
+++ backends/imgui_impl_dx11.cpp
@@ -455,32 +455,36 @@ bool    ImGui_ImplDX11_CreateDeviceObjects()
 
     // Create the pixel shader
     {
-        static const char* vertexShader =
-            "cbuffer vertexBuffer : register(b0) \
+        static const char* pixelShader = g_NeedColorFixing ?
+            "struct PS_INPUT\
             {\
-              float4x4 ProjectionMatrix; \
+            float4 pos : SV_POSITION;\
+            float4 col : COLOR0;\
+            float2 uv  : TEXCOORD0;\
             };\
-            struct VS_INPUT\
-            {\
-              float2 pos : POSITION;\
-              float4 col : COLOR0;\
-              float2 uv  : TEXCOORD0;\
-            };\
+            sampler sampler0;\
+            Texture2D texture0;\
             \
-            struct PS_INPUT\
+            float4 main(PS_INPUT input) : SV_Target\
             {\
-              float4 pos : SV_POSITION;\
-              float4 col : COLOR0;\
-              float2 uv  : TEXCOORD0;\
+            float4 out_col = input.col * texture0.Sample(sampler0, input.uv);\
+            out_col.xyz = pow(out_col.xyz, 2.2);\
+            return out_col;\
+            }"
+            :
+            "struct PS_INPUT\
+            {\
+            float4 pos : SV_POSITION;\
+            float4 col : COLOR0;\
+            float2 uv  : TEXCOORD0;\
             };\
+            sampler sampler0;\
+            Texture2D texture0;\
             \
-            PS_INPUT main(VS_INPUT input)\
+            float4 main(PS_INPUT input) : SV_Target\
             {\
-              PS_INPUT output;\
-              output.pos = mul( ProjectionMatrix, float4(input.pos.xy, 0.f, 1.f));\
-              output.col = input.col;\
-              output.uv  = input.uv;\
-              return output;\
+            float4 out_col = input.col * texture0.Sample(sampler0, input.uv);\
+            return out_col;\
             }";
 
         ID3DBlob* pixelShaderBlob;
